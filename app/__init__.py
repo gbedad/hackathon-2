681de06939d6442f25d0a94f8ee185fb4aeb79ea
gbedad <gerald.berrebi@gmail.com>
2022-06-26 19:45:49 +0200

    commit __init__ file

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from .utils import configuration
+
+# Flask object
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+flask_app = Flask(__name__)
+flask_app.config['SECRET_KEY'] = configuration.SECRET_KEY
+
+# Database connection
+db_info = {
+    'host': 'localhost',
+    'database': 'teaching_rooms',
+    'user': 'geraldberrebi',
+    'port': ''
+}
+
+flask_app.config[
+    'SQLALCHEMY_DATABASE_URI'] = f"postgresql://{db_info['user']}@{db_info['host']}/{db_info['database']}"
+
+flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(flask_app)
+migrate = Migrate(flask_app, db)
